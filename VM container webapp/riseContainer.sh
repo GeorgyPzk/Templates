99914274863532157dd63067b317068f766f39e2
georgy <georgyVSwork@yandex.ru>
2023-02-21 13:44:55 +0100

    add template VM container webapp

---
+++ VM container webapp/riseContainer.sh
@@ -0,0 +1,2 @@
+sudo docker build -t mywebapp:v1 .
+sudo docker run -d -p 80:80 mywebapp:v1
---
+++ VM container webapp/webservice-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#install apache2
+# apt-get -y update
+# apt-get -qq -y install apache2
+# create helloworld site
+echo "Hello from ${DEVOPS}!" > /var/www/html/index.html
